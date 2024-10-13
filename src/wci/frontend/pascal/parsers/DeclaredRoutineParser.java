4f7c20e26c51ad3ff55f58980a6088ae6398e476
Joshua Ewulo <jewulo@gmail.com>
2024-10-13 16:02:24 +0100

    Continue from Page 386. Chapter 11: Parsing Procedures and Functions Declarations.
    parseRoutineName
    parseHeader

--- src/wci/frontend/pascal/parsers/DeclaredRoutineParser.java
+++ src/wci/frontend/pascal/parsers/DeclaredRoutineParser.java
@@ -2,10 +2,21 @@ package wci.frontend.pascal.parsers;
 
 import wci.frontend.Token;
 import wci.frontend.TokenType;
+import wci.frontend.pascal.PascalErrorCode;
 import wci.frontend.pascal.PascalParserTD;
 import wci.frontend.pascal.PascalTokenType;
-import wci.intermediate.Definition;
-import wci.intermediate.SymTabEntry;
+import wci.intermediate.*;
+import wci.intermediate.symtabimpl.DefinitionImpl;
+
+import java.util.ArrayList;
+
+import static wci.frontend.pascal.PascalErrorCode.ALREADY_FORWARDED;
+import static wci.frontend.pascal.PascalErrorCode.MISSING_SEMICOLON;
+import static wci.frontend.pascal.PascalTokenType.IDENTIFIER;
+import static wci.frontend.pascal.PascalTokenType.SEMICOLON;
+import static wci.intermediate.symtabimpl.RoutineCodeImpl.DECLARED;
+import static wci.intermediate.symtabimpl.RoutineCodeImpl.FORWARD;
+import static wci.intermediate.symtabimpl.SymTabKeyImpl.*;
 
 /**
  * <h1>DeclaredRoutineParser</h1>
@@ -45,23 +56,137 @@ public class DeclaredRoutineParser extends DeclarationsParser
         switch ((PascalTokenType) routineType) {
 
             case PROGRAM: {
+                token = nextToken();    // consume PROGRAM
+                routineDefn = DefinitionImpl.PROGRAM;
+                dummyName = "DummyProgramName".toLowerCase();
                 break;
             }
 
             case PROCEDURE: {
+                token = nextToken();    // consume PROCEDURE
+                routineDefn = DefinitionImpl.PROCEDURE;
+                dummyName = "DummyProcedureName_".toLowerCase() +
+                                String.format("%03d", ++dummyCounter);
                 break;
             }
 
             case FUNCTION: {
+                token = nextToken();    // consume FUNCTION
+                routineDefn = DefinitionImpl.FUNCTION;
+                dummyName = "DummyFunctionName_".toLowerCase() +
+                                String.format("%03d", ++dummyCounter);
                 break;
             }
 
             default: {
+                routineDefn = DefinitionImpl.PROGRAM;
+                dummyName = "DummyProgramName".toLowerCase();
                 break;
             }
-
-
         }
-        return parentId;
+
+        // Parse the routine name.
+        routineId = parseRoutineName(token,dummyName);
+        routineId.setDefinition(routineDefn);
+
+        token = currentToken();
+
+        // Create new intermediate code for the routine.
+        ICode iCode = ICodeFactory.createICode();
+        routineId.setAttribute(ROUTINE_ICODE, iCode);
+        routineId.setAttribute(ROUTINE_ROUTINES, new ArrayList<SymTabEntry>());
+
+        // Push the routine's new symbol unto the stack.
+        // If it was forwarded push its existing symbol table.
+        if (routineId.getAttribute(ROUTINE_CODE) == FORWARD) {
+            SymTab symTab = (SymTab) routineId.getAttribute(ROUTINE_SYMTAB);
+            symTabStack.push(symTab);
+        }
+        else {
+            routineId.setAttribute(ROUTINE_SYMTAB, symTabStack.push());
+        }
+
+        // Program: Set the program identifier in the symbol table stack.
+        if (routineDefn == DefinitionImpl.PROGRAM) {
+            symTabStack.setProgramId(routineId);
+        }
+        // Non-forwarded procedure or function: Append to the parent's list of routines.
+        else if (routineId.getAttribute(ROUTINE_CODE) != FORWARD) {
+            ArrayList<SymTabEntry> subroutines = (ArrayList<SymTabEntry>)
+                    parentId.getAttribute(ROUTINE_ROUTINES);
+            subroutines.add(routineId);
+        }
+
+        // If the routine was forwarded, there should not be
+        // any formal parameters or a function return type.
+        // But parse anyway them if they're there.
+        if (routineId.getAttribute(ROUTINE_CODE) == FORWARD) {
+            if (token.getType() != SEMICOLON) {
+                errorHandler.flag(token, ALREADY_FORWARDED, this);
+                parseHeader(token, routineId);
+            }
+        }
+        // Parse the routine's formal parameters and function return type.
+        else {
+            parseHeader(token, routineId);
+        }
+
+        // Look for the semicolon.
+        token = currentToken();
+        if (token.getType() == SEMICOLON) {
+            do {
+                token = nextToken();    // consume ;
+            } while (token.getType() == SEMICOLON);
+        }
+        else {
+            errorHandler.flag(token, MISSING_SEMICOLON, this);
+        }
+
+        // Parse the routine's block or forward declaration.
+        if ((token.getType() == IDENTIFIER) &&
+            (token.getText().equalsIgnoreCase("forward")))
+        {
+            token = nextToken();    // consume forward
+            routineId.setAttribute(ROUTINE_CODE, FORWARD);
+        }
+        else {
+            routineId.setAttribute(ROUTINE_CODE, DECLARED);
+
+            BlockParser blockParser = new BlockParser(this);
+            ICodeNode rootNode = blockParser.parse(token, routineId);
+            iCode.setRoot(rootNode);
+        }
+
+        // Pop the routine's symbol table off the stack.
+        symTabStack.pop();
+
+        return routineId;
     }
+
+    /**
+     * Parse a routine's name.
+     * @param token the current token.
+     * @param dummyName
+     * @return
+     * @throws Exception if an error occurred.
+     */
+    private SymTabEntry parseRoutineName(Token token, String dummyName)
+        throws Exception
+    {
+        SymTabEntry routineId = null;
+
+        return routineId;
+    }
+
+    /**
+     * Parse a routine's formal parameter list and the function return type.
+     * @param token
+     * @param routineId
+     * @throws Exception if an error occurred.
+     */
+    private void parseHeader(Token token, SymTabEntry routineId)
+        throws Exception
+    {
+    }
+
 }
